80ff247540d4e730ca97a5da3485ee605ddaf316
Khaleb Denham <60304297+KhalebDenham@users.noreply.github.com>
2025-06-10 11:53:05 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -29,16 +29,70 @@ $(document).ready(function() { //tells document to wait until it is ready to hid
     $("#alertDraw").hide();
 });
 
-const winningOutcomes = [ //array of winning outcomes
-    [box0, box1, box2], [box3, box4, box5], [box6, box7, box8], //horizontal rows
-    [box0, box4, box8], [box2, box4, box6], //diagonal
-    [box0, box3, box6], [box1, box4, box7], [box2, box5, box8] //vertical columns
-]
 
 //keep track of whos turn it is/current player
 let currentPlayer=" ";
 
 
+const winningOutcomes = [ //array of winning outcomes
+    [box0, box1, box2], [box3, box4, box5], [box6, box7, box8], //horizontal rows
+    [box0, box4, box8], [box2, box4, box6], //diagonal
+    [box0, box3, box6], [box1, box4, box7], [box2, box5, box8] //vertical columns
+];
+
+const endGame = () => {
+    console.log("Game Over!");
+
+    ${".box"}.css('pointer-events', "none"); //calls out all objects with box class and makes them not clickable;
+
+    ${"#p1"}.removeClass("bg-light border border-info");
+    ${"#p2"}.removeClass("bg-light border border-info");
+};
+
+const checkWinner = (currentPlayer, a, b, c) => {
+
+    if(a.text() === currentPlayer && b.text() === currentPlayer && c.text() === currentPlayer ) {
+        winner = true;
+
+        console.log(`Found winner, it is the ${currentPlayer}!`);
+
+        a.removeClass('text-info bg-dark');
+        a.addClass('text-dark bg-info');
+        b.removeClass('text-info bg-dark');
+        b.addClass('text-dark bg-info');
+        c.removeClass('text-info bg-dark');
+        c.addClass('text-dark bg-info');
+
+        if(currentPlayer === "X") {
+            currentPlayer = "player 1";
+        } else {
+            currentPlayer = "player 2";
+        }
+
+        ${"alert"}.text(`Game Over! ${currentPlayer}`);
+        ${"alertWinner"}.show();
+
+
+        endGame();
+
+    }
+};
+
+const checkOutcomes = () => {
+    checkWinner(currentPlayer, ...winningOutcomes(0)); //spreads the elementings of winning outcomes between the a b and c conditionals for the checkWinner function
+    checkWinner(currentPlayer, ...winningOutcomes(1)); //does so for all 9 elements in the array
+    checkWinner(currentPlayer, ...winningOutcomes(2));
+    checkWinner(currentPlayer, ...winningOutcomes(3));
+    checkWinner(currentPlayer, ...winningOutcomes(4));
+    checkWinner(currentPlayer, ...winningOutcomes(5));
+    checkWinner(currentPlayer, ...winningOutcomes(6));
+    checkWinner(currentPlayer, ...winningOutcomes(7));
+
+    if (turn === 9 && winner === false) {
+        endGame();
+        ${"#alertDraw"}.show();
+    } 
+};
 
 const startGame = () => {
     console.log("Start Game!");
@@ -46,8 +100,8 @@ const startGame = () => {
     currentPlayer = player1;
     console.log(currentPlayer);
 
+    ${'#p1'}.addClass("bg-light border border-info"); //creates class border and adds descriptive info for player1 
 
-        
 
 
     // Show start alert here
@@ -62,16 +116,25 @@ const startGame = () => {
         if(turn > 4) {
             //check for winner of game
             console.log("winner?");
+            checkOutcomes();
+        }
+
+        if (winner === false) {
+
         }
         
-        if(currentPlayer === player1) {
-            currentPlayer = player2;
-            console.log(turn++);
-        } else {
-            currentPlayer = player1;
+        if(currentPlayer === player1) { //if current player is equal truly to player1 
+            currentPlayer = player2; //become player 2
+            console.log(turn++); //turn increment once
+            ${'#p2'}.addClass("bg-light border border-info"); //adding class to the player 2 class
+            ${'#p1'}.removeClass("bg-light border border-info");
+        } else { //otherwise
+            currentPlayer = player1; //currentplayer becomes player1
+            ${'#p1'}.addClass("bg-light border border-info");
+            ${'#p2'}.removeClass("bg-light border border-info");
         }
     })
-}
+};
 
 
 // event listener
